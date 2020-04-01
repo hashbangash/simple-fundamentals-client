d4debe1be3aca7dbf4941594e7babd24cc519fd7
ashtreehugs <agrev8@gmail.com>
2020-04-01 14:23:22 -0400

    Add Open Dyslexic font, styled-components & styles
    Add custom styles to buttons, boxes/cards, h1, body, p

--- src/components/routes/Cards.js
+++ src/components/routes/Cards.js
@@ -2,6 +2,17 @@ import React, { useState, useEffect, Fragment } from 'react'
 // import { Link } from 'react-router-dom'
 import axios from 'axios'
 import apiUrl from '../../apiConfig'
+import styled from 'styled-components'
+
+const Box = styled.div`
+  margin: 1rem;
+  padding: 1rem;
+  border-radius: 2px 2px 0 0;
+  box-shadow: 0 2px 2px 0 rgba(0,0,0,0.14), 0 3px 1px -2px rgba(0,0,0,0.12), 0 1px 5px 0 rgba(0,0,0,0.2);
+`
+const Button = styled.button`
+  margin: 0 0.25rem 0 0.25rem;
+`
 
 const Cards = props => {
   const [cards, setCards] = useState([])
@@ -14,15 +25,16 @@ const Cards = props => {
 
   // <Link to={`/cards/${card.id}`}>{card.word}</Link>
   const cardsJSX = cards.map(card => (
-    <div key={card.id} className="box">
+    <Box key={card.id}>
       <h2 className="word">
         {card.word}
       </h2>
       <p className="definition">
         {card.definition}
       </p>
-      <button className="btn btn-primary" data-id={card.id}>Like</button>
-    </div>
+      <Button className="btn btn-primary btn-sm like" data-id={card.id}>Like</Button>
+      <Button className="btn btn-secondary btn-sm comment" data-id={card.id}>Comment</Button>
+    </Box>
   ))
 
   return (
